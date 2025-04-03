609887eb9382dd6a7588e9f33774e5e9f239578c
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2025-04-03 15:55:15 -0300

    fix: Corrige ReferenceError

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const axios = require('axios');
-const Parser = require('rss-parser');
-const AWS = require('aws-sdk');
-const fs = require('fs');
+import express from 'express';
+import axios from 'axios';
+import Parser from'rss-parser';
+import AWS from 'aws-sdk';
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
