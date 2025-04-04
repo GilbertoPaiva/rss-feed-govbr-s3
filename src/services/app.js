1b4a880c85476c5c8d21e264f9d40c8043736539
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2025-04-04 16:07:25 -0300

    build: Cria app.js

---
+++ src/services/app.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import express from "express";
+import rssRoute from "../routes/rssRoutes.js";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static("public"));
+
+app.use (express.json());
+
+app.use ("/api, rssRoute");
+
+export default app;
