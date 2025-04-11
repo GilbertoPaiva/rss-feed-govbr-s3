9fb85a4d8b2259a9a457524596679e5e714bd51e
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2025-04-11 17:29:00 -0300

    refactor: Altera URL de login para novo domínio

--- src/services/rssService.js
+++ src/services/rssService.js
@@ -1,17 +1,25 @@
+import Parser from 'rss-parser';
+const parser = new Parser();
+
 class RSSService {
-    static async fetchRSSFeed(url) {
+  static async fetchRSSFeed() {
+    const url = 'https://www.gov.br/pt-br/noticias/RSS'; 
 
-        const feed = await parser.parseURL(url);
+    const feed = await parser.parseURL(url);
 
-        const newData = {
-            title: feed.title,
-            items: feed.items.map((item) => ({
-                title: item.title,
-                link: item.link,
-                description: item.content || "Descrição indisponível.",
-                putDate: item.pubDate,
-                image: item.enclosuse ? item.enclosure.url : "Sem imagem",
-            })),
-        };
-    }
+    const newData = {
+      title: feed.title,
+      items: feed.items.map((item) => ({
+        title: item.title,
+        link: item.link,
+        description: item.content || "Descrição indisponível.",
+        pubDate: item.pubDate,
+        image: item.enclosure ? item.enclosure.url : "Sem imagem",
+      })),
+    };
+
+    return newData;
+  }
 }
+
+export default RSSService;
