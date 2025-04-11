9c6dbb3654da3e25af71274741b557847397eb86
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2025-04-11 17:32:29 -0300

    refactor: Altera URL de login para novo domínio

--- index.js
+++ index.js
@@ -1,37 +1,13 @@
 import express from 'express';
-import axios from 'axios';
-import Parser from'rss-parser';
-import AWS from 'aws-sdk';
-import fs from 'fs';
+import cors from 'cors';
+import router from './routes/rssRoutes.js';
 
 const app = express();
 const port = 3000;
 
-const parser = new Parser();
-const s3 = new AWS.S3({ region: 'us-east-1' });
-const bucketName = 'seu-bucket-s3';
-
-app.get('/extract', async (req, res) => {
-  try {
-    const feed = await parser.parseURL('https://www.gov.br/pt-br/rss');
-    const jsonData = JSON.stringify(feed, null, 2);
-
-    const params = {
-      Bucket: bucketName,
-      Key: 'rss-data.json',
-      Body: jsonData,
-      ContentType: 'application/json',
-    };
-
-    await s3.upload(params).promise();
-
-    res.send('RSS extraído e salvo no S3 com sucesso.');
-  } catch (error) {
-    console.error('Erro ao processar RSS:', error);
-    res.status(500).send('Erro ao processar RSS.');
-  }
-});
-
+app.use(cors());
+app.use(express.json());
+app.use('/api', router); 
 app.use(express.static('public'));
 
 app.listen(port, () => {
