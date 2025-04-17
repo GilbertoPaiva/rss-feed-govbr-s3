44db9a7fd8cf007af33bf73a5744384373a9783b
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2025-04-17 17:47:56 -0300

    refactor: Altera rota de '/extract' para '/buscar-rss'

--- src/routes/rssRoutes.js
+++ src/routes/rssRoutes.js
@@ -3,6 +3,6 @@ import { extractAndSaveRSS } from '../controllers/rssController.js';
 
 const router = express.Router();
 
-router.get('/extract', extractAndSaveRSS);
+router.get('/buscar-rss', extractAndSaveRSS);
 
 export default router;
