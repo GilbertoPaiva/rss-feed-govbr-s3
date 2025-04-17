e4e475913ce6aeef80c60a76b343e218412abfa2
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2025-04-17 16:24:05 -0300

    fix: middleware and route syntax in app.js

--- src/services/app.js
+++ src/services/app.js
@@ -1,15 +1,16 @@
 import 'dotenv/config';
 import express from "express";
+import cors from 'cors';
 import rssRoute from "../routes/rssRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use (cors());
 
-app.use(express.static("public"));
+app.use (express.static("public"));
 
 app.use (express.json());
 
-app.use ("/api, rssRoute");
+app.use ("/api", rssRoute);
 
 export default app;
